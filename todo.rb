52c9f8f1a3536e5f39ff3cdb51c567414487b3df
astout55555 <alex.the.stout5@gmail.com>
2023-04-28 16:50:15 -0700

    Edited based on solution for the current assignment within the project.

--- todo.rb
+++ todo.rb
@@ -21,19 +21,6 @@ get "/lists" do
   erb :lists
 end
 
-get "/lists/:id" do
-  if params[:id] == 'new'
-    erb :new_list # render the new list form
-  elsif (0...session[:lists].size).cover?(params[:id].to_i)
-    @list = session[:lists][params[:id].to_i]
-    @todos = @list[:todos]
-    erb :list_details # show list details for selected list
-  else
-    session[:error] = "The specified list could not be found."
-    redirect "/lists"
-  end
-end
-
 # Return an error message if the name is invalid. Return nil if name is valid.
 def error_for_list_name(name)
   if !(1..100).cover?(name.size)
@@ -54,6 +41,24 @@ post "/lists" do
   else
     session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list 
+end
+# Placed above the next route to avoid `:id` being set as 'new'
+
+get "/lists/:id" do
+  id = params[:id].to_i
+  if (0...session[:lists].size).cover?(id)
+    @list = session[:lists][id]
+    @todos = @list[:todos]
+    erb :list_details # show list details for selected list
+  else
+    session[:error] = "The specified list could not be found."
     redirect "/lists"
   end
 end
