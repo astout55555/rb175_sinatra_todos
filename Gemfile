f7e72e7fb96c346378c54430932a8976a490b4fc
astout55555 <alex.the.stout5@gmail.com>
2023-05-07 18:45:23 -0700

    Finished the todos project! Time to commit and then deploy to heroku.

--- Gemfile
+++ Gemfile
@@ -4,3 +4,7 @@ ruby "3.1.0"
 gem "sinatra", "~>1.4.7"
 gem "sinatra-contrib"
 gem "erubis"
+
+group :production do
+  gem "puma"
+end
--- todo.rb
+++ todo.rb
@@ -1,25 +1,33 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" if development?
 require "tilt/erubis"
 require "sinatra/content_for"
+
+=begin
+
+### Data Structure Notes ###
+
+# session {
+#   lists: [array of list hashes] # list_id based on array position
+# }
+  # list_hash {
+  #   name: string
+  #   todos: [array of todo hashes] # todo_id based on array position
+  # }
+    # todo_hash {
+    #   name: string
+    #   completed: boolean
+    # }
+
+=end
 
 configure do
   enable :sessions
   set :session_secret, 'secret'
 end
 
-before do
-  session[:lists] ||= []
-  @lists = session[:lists]
-end
-
-get "/" do
-  redirect "/lists"
-end
-
-# View list of lists
-get "/lists" do
-  erb :lists
+not_found do
+  "<html><body><h1>Not Found</h1></body></html>"
 end
 
 # Return an error message if the name is invalid. Return nil if name is valid.
@@ -29,6 +37,80 @@ def error_for_list_name(name)
   elsif session[:lists].any? { |list| list[:name] == name }
     "List name must be unique."
   end
+end
+
+def list?(item)
+  session[:lists].include?(item)
+end
+
+helpers do
+  def list_complete?(list)
+    !list[:todos].empty? && todos_remaining_count(list) == 0
+  end
+
+  def todos_remaining_count(list)
+    todos = list[:todos]
+    todos.reject { |todo| todo[:completed] }.size
+  end
+
+  def list_class(list)
+    "complete" if list_complete?(list)
+  end
+
+  def todo_class(todo)
+    "complete" if todo[:completed]
+  end
+
+  # my solution before I saw LS solution:
+  # general for both lists and todos, but that pushes it over the 10 line limit
+  # (rubocop gets mad). it also uses a `list?` helper method for clarity,
+  # but the code is just one line anyway: `session[:lists].include?(item)`.
+  # def id_and_sort_by_completion(lists_or_todos)
+  #   identified = {}
+
+  #   lists_or_todos.each_with_index do |item, id|
+  #     identified[item] = id
+  #   end
+
+  #   identified.sort_by do |item, _|
+  #     if list?(item)
+  #       list_complete?(item) ? 1 : 0
+  #     else
+  #       item[:completed] ? 1 : 0
+  #     end
+  #   end
+  # end
+
+  # my final version, combining the 2 LS methods:
+  # yields lists or todos to block in correct order,
+  # doesn't require calling #each on the return value.
+  # rubocop flags the unused explicit block param `&block` so I dropped it.
+  def sort_and_display(lists_or_todos)
+    complete, incomplete = lists_or_todos.partition do |item|
+      if session[:lists].include?(item) # if list
+        list_complete?(item)
+      else # if todo
+        item[:completed]
+      end
+    end
+
+    incomplete.each { |item| yield item, lists_or_todos.index(item) }
+    complete.each { |item| yield item, lists_or_todos.index(item) }
+  end
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View list of lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists
 end
 
 # Create a new list
@@ -48,12 +130,12 @@ end
 
 # Render the new list form
 get "/lists/new" do
-  erb :new_list 
+  erb :new_list
 end
 # Placed above the next route to avoid `:id` being set as 'new'
 
-get "/lists/:id" do
-  @list_id = params[:id].to_i
+get "/lists/:list_id" do
+  @list_id = params[:list_id].to_i
   if (0...session[:lists].size).cover?(@list_id)
     @list = session[:lists][@list_id]
     @todos = @list[:todos]
@@ -65,8 +147,8 @@ get "/lists/:id" do
 end
 
 # Render the list edit form
-get "/lists/:id/edit" do
-  @list_id = params[:id].to_i
+get "/lists/:list_id/edit" do
+  @list_id = params[:list_id].to_i
   if (0...session[:lists].size).cover?(@list_id)
     @list = session[:lists][@list_id]
     erb :edit_list # show form to edit selected list name
@@ -77,10 +159,10 @@ get "/lists/:id/edit" do
 end
 
 # Edit the list title
-post "/lists/:id" do
+post "/lists/:list_id" do
   list_name = params[:list_name].strip
 
-  @list_id = params[:id].to_i
+  @list_id = params[:list_id].to_i
   @list = session[:lists][@list_id]
 
   error = error_for_list_name(list_name)
@@ -95,16 +177,15 @@ post "/lists/:id" do
 end
 
 # Delete the list
-post "/lists/:id/delete" do
-  session[:lists].delete_at(params[:id].to_i)
+post "/lists/:list_id/delete" do
+  session[:lists].delete_at(params[:list_id].to_i)
   session[:success] = "The list has been deleted."
   redirect "/lists"
 end
 
 def error_for_todo(name)
-  if !(1..100).cover?(name.size)
-    "Todo must be between 1 and 100 characters."
-  end
+  return unless !(1..100).cover?(name.size)
+  "Todo must be between 1 and 100 characters."
 end
 
 # Add a todo item to a list
@@ -119,7 +200,47 @@ post "/lists/:list_id/todos" do
     erb :list_details
   else
     session[:success] = "The todo was added."
-    @list[:todos] << {name: name, completed: false}
+    @list[:todos] << { name: name, completed: false }
     redirect "/lists/#{@list_id}"
   end
+end
+
+# Delete a todo item from the list
+post "/lists/:list_id/todos/:todo_id/delete" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  todo_id = params[:todo_id].to_i
+
+  @list[:todos].delete_at(todo_id)
+  session[:success] = "The todo has been deleted."
+  redirect "/lists/#{@list_id}"
+end
+
+# Update completion status of a todo item
+post "/lists/:list_id/todos/:todo_id/check" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  todo = @list[:todos][params[:todo_id].to_i]
+
+  if params[:completed] == 'false'
+    todo[:completed] = false
+  elsif params[:completed] == 'true'
+    todo[:completed] = true
+  end
+
+  session[:success] = "The todo has been updated."
+  redirect "/lists/#{@list_id}"
+end
+
+# Complete all todos
+post "/lists/:list_id/complete_all" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  @list[:todos].each do |todo|
+    todo[:completed] = true
+  end
+
+  session[:success] = "All todos have been completed."
+  redirect "/lists/#{@list_id}"
 end
